1be10c2ef9fdd515964b3b50ba2c43d976517171
mtrebing <mtrebing@gmail.com>
2016-12-01 18:37:54 -0500

    date of revision added

--- testfile.R
+++ testfile.R
@@ -1,15 +1,5 @@
 # This is a test script
-a <- rnorm(200)
-plot(a)
 # new comment
 a <- rnorm(100)
 plot(a)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
 # just a second note
->>>>>>> e738fe54e515e752ec8a04a4023c3b85707ba5c8
->>>>>>> dbf1199c61a795b07098ccbd6838279b408f3807
->>>>>>> 40362aaa24338293f1b56afb1807fe4357001e7d
